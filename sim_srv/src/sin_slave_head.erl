db336e9d20076db8ed79da006cdd321cbbcd034d
Minified <gora.pub@gmail.com>
2017-12-22 13:16:02 +0100

    Added leash_cast support.

--- sim_srv/src/sin_slave_head.erl
+++ sim_srv/src/sin_slave_head.erl
@@ -8,9 +8,13 @@
 -export([rise/0, rise/1]).
 -export([find_master/1]).
 
+-include("./sin_task.hrl").
+
 -record(state,{
   leash_pid :: pid(),
-  leash_ref :: reference()
+  leash_ref :: reference(),
+  running_tasks :: [sin_running_task()],
+  tasks_w8ing_4_modules :: [sin_task()]
 }).
 
 % ---
@@ -23,6 +27,12 @@ handle_cast({find_master}, State) ->
   Ref = erlang:make_ref(),
   Pid = erlang:spawn_link(sin_slave_leash, init, [self(), Ref]),
   {noreply, State#state{leash_pid=Pid,leash_ref=Ref}};
+
+handle_cast({sin_slave_leash, LeashRef, MessageFromLeash}, State) ->
+  case State#state.leash_ref of
+    LeashRef -> leash_cast(MessageFromLeash, State);
+    _ -> {noreply, State}
+  end;
 
 handle_cast(Request, State) ->
   io:format("~p:~p: ~p~n", [?MODULE, ?FUNCTION_NAME, Request]),
@@ -54,3 +64,22 @@ rise(Options) ->
 
 find_master(SlaveHead) ->
   gen_server:cast(SlaveHead, {find_master}).
+
+% ---
+
+leash_cast({run_task, Task}, State) when erlang:is_list(Task) ->
+  case sin_dep:filter_loaded(Task#sin_task.dependencies) of
+    [] -> 
+      RunningTasks = State#state.running_tasks ++ [run_task(Task)],
+      {noreply, State#state{running_tasks=RunningTasks}};
+    Modules ->
+      State#state.leash_pid ! {send_to_master, {i_need_modules, Modules}},
+      TasksW8 = State#state.tasks_w8ing_4_modules ++ [Task],
+      {noreply, State#state{tasks_w8ing_4_modules = TasksW8}}
+  end;
+
+leash_cast(_Msg, State) ->
+  {noreply, State}.
+
+run_task(Task) ->
+  #sin_running_task{}.
